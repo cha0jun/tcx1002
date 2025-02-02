f42e184a7dbdc9345e1a7aa0dffd16059de95462
cha0jun <matthewcj.tan@gmail.com>
2025-02-02 23:27:41 +0800

    assignment 1

--- assignment1/t2.py
+++ assignment1/t2.py
@@ -1,4 +1,6 @@
-def min_workers(demand:list):
+def find_highest_window(demand:list):
+    demandlist = [] ## create a copy to prevent state change of original input
+    demandlist.extend(demand)
     highest_tot = []
     '''
        mon-fri [0:5]
@@ -9,13 +11,36 @@ def min_workers(demand:list):
        sat-wed [5: next 2]
        sun-thurs [6: next 3]
     ''' 
-    demand.extend(demand[0:3])
-    
+    demandlist.extend(demandlist[0:3])
     idx = 0
     while idx < 7:
-        highest_tot.append(sum(demand[idx:idx+5])) ## list of days with the total number of demand
-        idx += 1
-    print(highest_tot)
+        highest_tot.append(sum(demandlist[idx:idx+5])) ## list of total demand of each 5 day window
+        idx += 1  
+    position = highest_tot.index(max(highest_tot))
+    return position
+
+
+def min_workers(demand:list):
+    worker_list = []
+    worker_count = 0
+    total_demand = sum(demand)
     
+    while total_demand > 0:
+        curr = find_highest_window(demand)
+        worker_list.append(curr)
+        worker_count += 1
+        for days in range(5): ## loop to reduce demand
+            idx = days + curr
+            if idx > 6:
+                idx = idx - 7
+            if demand[idx] > 0:
+                val = demand[idx] - 1
+                demand[idx] = val
+        total_demand = sum(demand)
+        
+    return (worker_count, worker_list)
     
 min_workers([4, 2, 4, 3, 5, 4, 6])
+        
+    
+    
