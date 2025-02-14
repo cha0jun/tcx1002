b789a5eeaa3c8faced32ad142f8920f19a21cff9
cha0jun <matthewcj.tan@gmail.com>
2025-02-14 22:34:41 +0800

    t4 solutions

--- tut4/t4.py
+++ tut4/t4.py
@@ -5,5 +5,36 @@ user_connections = { "Alice": ["Bob", "Charlie", "David", "Eve", "Frank"],
                      "Eve" : ["Alice","Bob"],
                      "Frank" : ["Bob"]}
                      
-def friend_suggestion(user_connections):
-    pass
+def get_mutuals(main:str, sub:str, connections:dict):
+    res = []
+    
+    main_connections = connections[main]
+    sub_connections = connections[sub]
+    
+    for i in sub_connections:
+        if i not in main_connections and i != main:
+            for x in connections[i]:
+                if x in main_connections:
+                    res.append(i)
+    return res
+
+
+                
+
+def friend_suggestion(user_connections:dict):
+    res = {}
+    for key in user_connections.keys():
+        res[key] = []
+        templist = []
+        for subkey in user_connections.keys():
+            if key != subkey:
+                templist.extend(get_mutuals(key,subkey,user_connections))
+        templist.sort()
+        for rec in templist:
+            if rec not in res[key]:
+                res[key].append(rec)
+            
+    return res
+    
+
+print(friend_suggestion(user_connections))
